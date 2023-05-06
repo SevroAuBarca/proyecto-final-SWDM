e62882de7ba85e6fec6a2b2609ae84142ffab55a
SevroAuBarca <daniel_contreras001@hotmail.com>
2023-05-05 22:29:49 -0600

    @SevroAuBarca: Page Routes Components Done

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './Pages/auth/auth.component';
+import { HomeComponent } from './Pages/home/home.component';
+import { JobComponent } from './Pages/job/job.component';
+import { ProfileComponent } from './Pages/profile/profile.component';
+import { MyFeedComponent } from './Pages/my-feed/my-feed.component';
+import { SettingsComponent } from './Pages/settings/settings.component';
 
-const routes: Routes = [];
+/*
+  routes:
+    -Auth
+    -Home
+    -JobDescription/:id
+    -Profile/:id
+    -MyProfile/:id
+    -ProfileSettings/:id
+*/
+
+const routes: Routes = [
+  { path: '', component: AuthComponent },
+  { path: 'Home', component: HomeComponent },
+  { path: 'Job/:id', component: JobComponent },
+  { path: 'Profile/:id', component: ProfileComponent },
+  { path: 'MyFeed/:id', component: MyFeedComponent },
+  { path: 'Settings/:id', component: SettingsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './Pages/auth/auth.component';
+import { HomeComponent } from './Pages/home/home.component';
+import { JobComponent } from './Pages/job/job.component';
+import { ProfileComponent } from './Pages/profile/profile.component';
+import { MyFeedComponent } from './Pages/my-feed/my-feed.component';
+import { SettingsComponent } from './Pages/settings/settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    HomeComponent,
+    JobComponent,
+    ProfileComponent,
+    MyFeedComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
